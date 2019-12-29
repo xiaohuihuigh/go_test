c337b531742f2e1485c1d16c5d4e7bed43dad2fb
xiaohuihuigh <xiaohui@shu.edu.cn>
2019-12-29 22:48:16 +0800

    on barch dev

--- ipcProject/ipc/client.go
+++ ipcProject/ipc/client.go
@@ -5,3 +5,12 @@ import "fmt"
 type IpcClient struct {
 	conn chan string
 }
+
+type Request struct {
+	Methond string `json:"method,string"`
+	Params  Param  `json:"params,Param"`
+}
+type Param struct {
+	K string `json:"k,string"`
+	V string `json:"v,string"`
+}
